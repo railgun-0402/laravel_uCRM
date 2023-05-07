35d5604f4200557449cf72e3a27c9faea3706e60
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-05-07 21:59:14 +0900

    update function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+    }
+);
+
+Route::get('/inertia-test/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia-test/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
